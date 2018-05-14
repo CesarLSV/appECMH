2e7a88b449ab1d88a59a2e12fc7fb98d9814e0f2
CesarLSV <cesarlopez1909@gmail.com>
2018-05-14 16:13:56 -0600

    appeccmh

--- appECMH/appECMH/Models/Notas.cs
+++ appECMH/appECMH/Models/Notas.cs
@@ -1,18 +1,45 @@
 ﻿namespace appECMH.Models
 {
-    class Notas
+    using System.Collections.Generic;
+    using Newtonsoft.Json;
+
+
+    public class Notas
     {
+        [JsonProperty(PropertyName = "PeopleID")]
         public string PeopleID { get; set; }
+
+        [JsonProperty(PropertyName = "LegalName")]
         public string LegalName { get; set; }
+
+        [JsonProperty("AcademicYear")]
         public string AcademicYear { get; set; }
+
+        [JsonProperty("AcademicTerm")]
         public string AcademicTerm { get; set; }
+
+        [JsonProperty("AcademicSession")]
         public string AcademicSession { get; set; }
+
+        [JsonProperty("EventID")]
         public string EventID { get; set; }
+
+        [JsonProperty("Section")]
         public string Section { get; set; }
+
+        [JsonProperty("EventLongName")]
         public string EventLongName { get; set; }
+
+        [JsonProperty("Carrera")]
         public string Carrera { get; set; }
+
+        [JsonProperty("PrimerParcial")]
         public object PrimerParcial { get; set; }
+
+        [JsonProperty("SegundoParcial")]
         public object SegundoParcial { get; set; }
+
+        [JsonProperty("EvaluacionFinal")]
         public object EvaluacionFinal { get; set; }
 
     }
