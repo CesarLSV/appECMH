4b3fbb74ee9a8ede180f2732a610fd9c75953bf6
CesarLSV <cesarlopez1909@gmail.com>
2018-05-21 15:50:05 -0600

    appecmh

--- appECMH/appECMH/Models/MHorarios.cs
+++ appECMH/appECMH/Models/MHorarios.cs
@@ -11,18 +11,6 @@ namespace appECMH.Models
         [JsonProperty(PropertyName = "PeopleID")]
         public string PeopleID { get; set; }
 
-        [JsonProperty(PropertyName = "FirstName")]
-        public string FirstName { get; set; }
-
-        [JsonProperty(PropertyName = "MiddleName")]
-        public string MiddleName { get; set; }
-
-        [JsonProperty(PropertyName = "LastName")]
-        public string LastName { get; set; }
-
-        [JsonProperty(PropertyName = "Suffix")]
-        public object Suffix { get; set; }
-
         [JsonProperty(PropertyName = "LegalName")]
         public string LegalName { get; set; }
 
@@ -38,50 +26,16 @@ namespace appECMH.Models
         [JsonProperty(PropertyName = "EventLongName")]
         public string EventLongName { get; set; }
 
-        [JsonProperty(PropertyName = "Credit")]
-        public double Credit { get; set; }
-
-        [JsonProperty(PropertyName = "StartTime")]
-        public DateTime StartTime { get; set; }
-
-        [JsonProperty(PropertyName = "EndTime")]
-        public DateTime EndTime { get; set; }
-
-        [JsonProperty(PropertyName = "MediumDesc")]
-        public string MediumDesc { get; set; }
-
-        [JsonProperty(PropertyName = "LongDesc")]
-        public string LongDesc { get; set; }
-
-        [JsonProperty(PropertyName = "Domingo")]
-        public string Domingo { get; set; }
-
-        [JsonProperty(PropertyName = "Lunes")]
-        public string Lunes { get; set; }
-
-        [JsonProperty(PropertyName = "Martes")]
-        public string Martes { get; set; }
-
-        [JsonProperty(PropertyName = "Miercoles")]
-        public string Miercoles { get; set; }
-
-        [JsonProperty(PropertyName = "Jueves")]
-        public string Jueves { get; set; }
-
-        [JsonProperty(PropertyName = "Viernes")]
-        public string Viernes { get; set; }
-
-        [JsonProperty(PropertyName = "Sabado")]
-        public string Sabado { get; set; }
-
         [JsonProperty(PropertyName = "Section")]
         public string Section { get; set; }
 
-        [JsonProperty(PropertyName = "RevisionDate")]
-        public DateTime RevisionDate { get; set; }
+        [JsonProperty(PropertyName = "Section2")]
+        public string Section2 { get; set; }
 
-        [JsonProperty(PropertyName = "RevisionTime")]
-        public DateTime RevisionTime { get; set; }
+        [JsonProperty(PropertyName = "HorariosProcesados")]
+        public string HorariosProcesados { get; set; }
+
+     
 
     }
 }
